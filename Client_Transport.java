778f30b615b3d4eb50ddc92527ed33da7aca6267
Emile Boutin <emile.boutin@videotron.ca>
2022-06-27 14:40:45 -0400

    small fix

--- Client_Transport.java
+++ Client_Transport.java
@@ -16,28 +16,30 @@ public class Client_Transport {
     public Client_Transport(byte [] NomByte, byte [] FichierByte) throws IOException {
         this.FichierByte=FichierByte;
         this.FichierNom = NomByte;
+         //String str = new String(FichierNom, StandardCharsets.UTF_8);
+      //  System.out.println(str);
     }
     public void run() throws IOException {
         int dernierPaquet = FichierByte.length/200;
-        byte[] nom = ByteBuffer.allocate(15).put(FichierNom).array();
+//        byte[] nom = ByteBuffer.allocate(15).put(FichierNom).array();
        byte [] compteurB = ByteBuffer.allocate(5).putInt(compteur).array();
        byte [] premier =ByteBuffer.allocate(5).putInt(0).array();
        byte [] dernier = ByteBuffer.allocate(5).putInt(dernierPaquet).array();
 
         ByteArrayOutputStream header = new ByteArrayOutputStream();
-        header.write(nom);
+        //header.write(nom);
         header.write(compteurB);
         header.write(premier);
         header.write(dernier);
         header.close();
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        temp= Arrays.copyOfRange(FichierNom,0,taille);
+        temp= Arrays.copyOfRange(FichierByte,0,taille);
         out.write(header.toByteArray());
         out.write(temp);
         out.close();
-       // String str = new String(out.toByteArray(), StandardCharsets.UTF_8);
-        //System.out.println("akkkkkkkkkkkk"+str);
+        //String str = new String(out.toByteArray(), StandardCharsets.UTF_8);
+      //  System.out.println("akkkkkkkkkkkk"+str);
         Liaison.run(out.toByteArray());
 
 for(int i=200;i<FichierByte.length;i=i+taille){
@@ -47,10 +49,12 @@ for(int i=200;i<FichierByte.length;i=i+taille){
     head.write(premier);
     head.write(dernier);
     head.close();
+
     temp = Arrays.copyOfRange(FichierByte,i,taille+i);
     head.write(temp);
-   // String str = new String(temp, StandardCharsets.UTF_8);
-   // System.out.println("\n\n\n\n de "+ i +"aaaaaaaaaa"+(taille+i)+str);
+
+ //  String str = new String(temp, StandardCharsets.UTF_8);
+  // System.out.println("\n\n\n\n de "+ i +"aaaaaaaaaa"+(taille+i)+str);
     Liaison.run(head.toByteArray());
     compteur++;
 
